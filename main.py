113fe770c32e3403aa2e2f0edfb474a24a0a85f2
Yuu Sakano <y.sakano.368@stn.nitech.ac.jp>
2026-05-14 17:08:41 +0900

    remove comments

--- main.py
+++ main.py
@@ -5,26 +5,6 @@ def calculate_weights(x, y):
     z = 2 * x - y * 0.99
     return z
 
-## equation modified
-# def calculate_weights(x, y):
-#     z = 2 * x - y
-#     return z
-
-## # a simple mistake ...
-## def calculate_weights(x, y):
-##     z = 2 * x - y + 1
-##     return z
-
-## # # new version
-## # def calculate_weights(x, y):
-## #     z = 2 * x - z + 1
-## #     return z
-
-#### def calculate_weights(x, y):
-####     z = 2 * x - y
-####     return z
-
-
 def apply_to_adjacent_pairs(a):
     for i in range(len(a) - 1):
         calculate_weights(a[i], a[i + 1])
